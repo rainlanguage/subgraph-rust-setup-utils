120f12f3b0cc4b30b458a3c45970c91c4c57490c
NanezX <victor.nanezj@gmail.com>
2023-12-07 18:42:50 -0400

    basic tests -  not related

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,4 @@
-pub fn add(left: usize, right: usize) -> usize {
+/// LOL
+pub fn add(left: i32, right: i32) -> i32 {
     left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
 }
